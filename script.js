fa49a3431f66135eb4f172e1c019d13ab74be9a2
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2022-02-17 16:50:34 -0600

    callback hell

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 
-const getCountry = (country) => {
+const renderCountry = (response, className = "") => {
+  const html = `
+  <article class="country ${className}">
+  <img class="country-img" src="${response.flags.png}" />
+  <div class="country-data">
+  <h3 class="country-name">${response.name.official}</h3>
+  <h4 class="country-region">${response.region}</h4>
+  <p class="country-row"><span>👫</span>${
+    (+response.population / 1000000).toFixed(1) + "m"
+  }</p>
+  <p class="country-row"><span>🗣️</span>${Object.keys(
+    response.languages
+  )[0].toUpperCase()}</p>
+  <p class="country-row"><span>💰</span>${
+    Object.keys(response.currencies)[0]
+  }</p>
+  </div>
+  </article>
+`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = (country) => {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
   request.send();
@@ -14,32 +38,40 @@ const getCountry = (country) => {
     let [response] = JSON.parse(request.responseText);
     console.log(response);
 
-    const html = `
-    <article class="country">
-    <img class="country-img" src="${response.flags.png}" />
-    <div class="country-data">
-    <h3 class="country-name">${response.name.official}</h3>
-    <h4 class="country-region">${response.region}</h4>
-    <p class="country-row"><span>👫</span>${
-      (+response.population / 1000000).toFixed(1) + "m"
-    }</p>
-    <p class="country-row"><span>🗣️</span>${Object.keys(
-      response.languages
-    )[0].toUpperCase()}</p>
-    <p class="country-row"><span>💰</span>${
-      Object.keys(response.currencies)[0]
-    }</p>
-    </div>
-    </article>
-`;
+    renderCountry(response);
 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
+    //Get neighbor country & render
+
+    const [neighbor] = response.borders;
+    if (!neighbor) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener("load", () => {
+      let [response2] = JSON.parse(request2.responseText);
+      console.log(response2);
+
+      renderCountry(response2, "neighbor");
+    });
   });
 };
 
-getCountry("usa");
-getCountry("canada");
-getCountry("ireland");
-getCountry("india");
-getCountry("mexico");
+setTimeout(() => {
+  console.log("1 second");
+  setTimeout(() => {
+    console.log("2 second");
+    setTimeout(() => {
+      console.log("3 second");
+      setTimeout(() => {
+        console.log("4 second");
+        setTimeout(() => {
+          console.log("Welcome to callback hell");
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+getCountryAndNeighbor("ireland");
